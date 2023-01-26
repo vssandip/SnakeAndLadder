0b3ca7a236bdb70f93a27ad3733f9c6ce8b86117
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-26 16:29:03 +0530

    [ Sandip ]- Start Position 0

---
+++ SnakeAndLadder/SnakeLadderUC1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    internal class SnakeLadderUC1
+    {
+        public static void Start()
+        {
+            //implementing single player start at 0.
+            //variables
+            int position = 0;
+            int playerOne;
+            //initialize player
+            playerOne = position;
+            Console.WriteLine($"player One position is {playerOne}");
+        }
+
+        public static void Board()
+        {
+            //at the start
+            Start();
+        }
+    }
+}
